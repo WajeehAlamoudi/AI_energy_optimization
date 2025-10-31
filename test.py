bb172274e0ab8b0284e6d4c52ea4bff3dc0135b6
WajeehAlamoudi <osamaandwajeeh@gmail.com>
2025-11-01 00:26:33 +0300

    wajeeh-update

--- test.py
+++ test.py
@@ -22,8 +22,6 @@ from lstm_predictor import LSTMPredictor
 from paths import DATA_DIR, LOGS_DIR, MODELS_DIR
 
 
-
-
 # === 🌍 SYSTEM INITIALIZATION ===
 def init_system():
     calibrator = ImpactCalibrator()
@@ -36,6 +34,7 @@ def init_system():
         "devices_count": len(devices.get_all_devices()),
         "homes_count": len(homes.homes)
     }
+
 
 gg = init_system()
 print(gg)
